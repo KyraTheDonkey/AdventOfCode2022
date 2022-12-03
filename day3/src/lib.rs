301e62a318cbc38df7a5d5d65aeee92858919079
KyraTheDonkey <kyrathedonkey@gmail.com>
2022-12-03 12:42:27 +0200

    Part 1 of Day 3

--- day3/src/lib.rs
+++ day3/src/lib.rs
@@ -1,4 +1,31 @@
-pub fn run(input: &String, part: i32) -> u64 {
+use substring::Substring;
 
-    return 0;
+pub fn run(input: &String, _part: i32) -> u32 {
+    let mut sum_priorty: u32 = 0;
+    let backpacks = input.split("\n");
+    for backpack in backpacks {
+        let len = backpack.len();
+        let first_half = backpack.substring(0, len/2);
+        let second_half = backpack.substring(len/2, len);
+        print!("{} {} ", first_half, second_half);
+
+        let mut duplicate_item: char = '_';
+        for char in first_half.chars() {
+            if second_half.contains(char) {
+                duplicate_item = char;
+                break;
+            }
+        }
+
+        let priority = match duplicate_item {
+            'a'..='z' => duplicate_item as u32 - 96,
+            'A'..='Z' => duplicate_item as u32 - 64 + 26,
+            _ => panic!("There wasn't a duplicate item in the backpack"),
+        };
+        println!("with duplicate \"{}\" and priority {}", duplicate_item, priority);
+        
+        sum_priorty += priority;
+    }
+
+    return sum_priorty;
 }
