f70ef252123b189f34158c1eab1ad70876fe243c
KyraTheDonkey <kyrathedonkey@gmail.com>
2022-12-03 16:40:26 +0200

    Part 2 of Day 3

--- day3/src/lib.rs
+++ day3/src/lib.rs
@@ -1,6 +1,14 @@
 use substring::Substring;
 
-pub fn run(input: &String, _part: i32) -> u32 {
+pub fn run(input: &String, part: i32) -> u32 {
+    match part {
+        1 => run_p1(input),
+        2 => run_p2(input),
+        _ => 0,
+    }
+}
+
+fn run_p1(input: &String) -> u32 {
     let mut sum_priorty: u32 = 0;
     let backpacks = input.split("\n");
     for backpack in backpacks {
@@ -27,5 +35,37 @@ pub fn run(input: &String, _part: i32) -> u32 {
         sum_priorty += priority;
     }
 
+    return sum_priorty;
+}
+
+fn run_p2 (input: &String) -> u32 {
+    let mut sum_priorty: u32 = 0;
+    let backpacks: Vec<&str> = input.split("\n").collect();
+    let backpacks_len = backpacks.len();
+    let mut i = 0;
+    while i + 2 < backpacks_len {
+        let elf_1 = backpacks[i];
+        let elf_2 = backpacks[i+1];
+        let elf_3 = backpacks[i+2];
+        print!("{} {} {}", elf_1, elf_2, elf_3);
+
+        let mut duplicate_item: char = '_';
+        for char in elf_1.chars() {
+            if elf_2.contains(char) && elf_3.contains(char) {
+                duplicate_item = char;
+                break;
+            }
+        }
+
+        let priority = match duplicate_item {
+            'a'..='z' => duplicate_item as u32 - 96,
+            'A'..='Z' => duplicate_item as u32 - 64 + 26,
+            _ => panic!("There wasn't a duplicate item in the backpack"),
+        };
+        println!("with duplicate \"{}\" and priority {}", duplicate_item, priority);
+        
+        sum_priorty += priority;
+        i += 3;
+    }
     return sum_priorty;
 }
