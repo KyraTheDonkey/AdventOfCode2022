cb5307331b8723b1ef1e4f2048ff65bf672c4767
KyraTheDonkey <kyrathedonkey@gmail.com>
2022-12-04 15:47:51 +0200

    Part 2 Day 4

--- day4/src/lib.rs
+++ day4/src/lib.rs
@@ -1,13 +1,25 @@
-pub fn run(input: &String, _part: i32) -> i32 {
+pub fn run(input: &String, part: i32) -> i32 {
     let split_input: Vec<&str> = input.split("\n").collect();
-    
+
     let mut total = 0;
     for entry in split_input {
-        let ( elf_1, elf_2 ) = read_elves(entry);
-        if (elf_1.lower_bound <= elf_2.lower_bound && elf_1.upper_bound >= elf_2.upper_bound) || 
-            (elf_1.lower_bound >= elf_2.lower_bound && elf_1.upper_bound <= elf_2.upper_bound) {
+        let (elf_1, elf_2) = read_elves(entry);
+        if part == 1 {
+            if (elf_1.lower_bound <= elf_2.lower_bound && elf_1.upper_bound >= elf_2.upper_bound)
+                || (elf_1.lower_bound >= elf_2.lower_bound && elf_1.upper_bound <= elf_2.upper_bound)
+            {
                 total += 1;
             }
+        } else {
+            let elf_1_range = elf_1.lower_bound..=elf_1.upper_bound;
+            let elf_2_range = elf_2.lower_bound..=elf_2.upper_bound;
+            for sector in elf_1_range {
+                if elf_2_range.contains(&sector) {
+                    total += 1;
+                    break;
+                }
+            }
+        }
     }
 
     return total;
@@ -15,7 +27,7 @@ pub fn run(input: &String, _part: i32) -> i32 {
 
 struct Elf {
     lower_bound: i32,
-    upper_bound: i32
+    upper_bound: i32,
 }
 
 impl Elf {
@@ -23,13 +35,22 @@ impl Elf {
         let s: Vec<&str> = input.split("-").collect();
         let lower_bound = match s[0].trim().parse() {
             Ok(num) => num,
-            Err(_) => panic!("The number read for the lower bound was not valid (tried {})", s[0]),
+            Err(_) => panic!(
+                "The number read for the lower bound was not valid (tried {})",
+                s[0]
+            ),
         };
         let upper_bound = match s[1].trim().parse() {
             Ok(num) => num,
-            Err(_) => panic!("The number read for the upper bound was not valid (tried {})", s[1]),
+            Err(_) => panic!(
+                "The number read for the upper bound was not valid (tried {})",
+                s[1]
+            ),
         };
-        Elf { lower_bound, upper_bound }
+        Elf {
+            lower_bound,
+            upper_bound,
+        }
     }
 }
 
