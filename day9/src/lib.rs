c6ba97caa32d3f863845c60cc9c8409741e300eb
KyraTheDonkey <kyrathedonkey@gmail.com>
2022-12-09 18:06:04 +0200

    Part 2 of Day 9

--- day9/src/lib.rs
+++ day9/src/lib.rs
@@ -1,10 +1,16 @@
 use core::fmt;
 
-pub fn run(input: &String, _part: i32) -> i32 {
+pub fn run(input: &String, part: i32) -> i32 {
     let mut head: Location = Location::new(0,0);
-    let mut tail: Location = Location::new(0,0);
     let mut visited_locations: Vec<Location> = Vec::new();
-    add_visited_location(&mut visited_locations, &tail);
+
+    let length: usize;
+    if part == 1 { length = 1} else { length = 9 };
+    let mut body_pieces: Vec<Location> = Vec::new();
+    for _i in 0..9 {
+        body_pieces.push(Location::new(0,0));
+    }
+    add_visited_location(&mut visited_locations, &body_pieces[length-1]);
 
 
     let split_input = input.split("\n");
@@ -18,9 +24,13 @@ pub fn run(input: &String, _part: i32) -> i32 {
         println!("Moving in direction {} {} times", direction, times);
         for _i in 0..times {
             move_head(&mut head, &direction);
-            move_tail(&head, &mut tail);
-            add_visited_location(&mut visited_locations, &tail);
-            println!("Head: {} Tail {}", head, tail);
+            move_tail(&head, &mut body_pieces[0]);
+            for i in 1..9 {
+                // Need to clone in the first argument so that I can put it into the function
+                move_tail(&body_pieces.get(i-1).unwrap().clone(), body_pieces.get_mut(i).unwrap());
+            }
+            add_visited_location(&mut visited_locations, &body_pieces[length-1]);
+            println!("Head: {} Tail {}", head, &body_pieces[length-1]);
         }
         
     }
